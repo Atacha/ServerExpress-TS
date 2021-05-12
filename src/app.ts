2940a99e89fdc0c58da4db79fad831b0a7207f4c
Cesar Ortz <cesarjavierortizmontero@gmail.com>
2021-05-11 23:53:33 -0500

    Task-21 GraphQL connection and simple schema

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,28 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { ApolloServer } from 'apollo-server-express';
+import compression from 'compression';
+import cors from 'cors';
+
+import Product from './models/products/ProductsSchema';
+
+dotenv.config();
 
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Well done!');
+const PORT = process.env.PORT;
+const server = new ApolloServer({
+  context: { Product },
+  playground: true,
+  introspection: true,
+  modules: [require('./models/products')],
 });
 
-app.listen(3000, () => {
-  console.log('The application is listening on port 3000!');
-});
+app.use(cors());
+app.use(compression());
+server.applyMiddleware({ app });
+
+app.listen(PORT, (): void =>
+  console.log(
+    `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
+  )
+);
