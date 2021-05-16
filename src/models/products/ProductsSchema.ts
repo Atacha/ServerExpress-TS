3c49e87a06e3b2044a4f9ea10795248860a2f449
Cesar Ortz <cesarjavierortizmontero@gmail.com>
2021-05-16 13:08:42 -0500

    TASK-13 Create a product schema

--- src/models/products/ProductsSchema.ts
+++ src/models/products/ProductsSchema.ts
@@ -2,9 +2,21 @@ import { Schema, model } from 'mongoose';
 
 const productSchema = new Schema({
   name: String,
-  description: String,
-  status: String,
+  mark: String,
+  category: { type: String, index: true },
+  subCategory: String,
+  model: String,
   price: Number,
+  avalible: Boolean,
+  stock: Number,
+  color: { type: String, index: true },
+  shortDescription: String,
+  longDescription: String,
+  weight: Number,
+  warranty: Boolean,
+  timeWarranty: Number,
+  inventory: Boolean,
+  hidden: Boolean,
 });
 
 const Product = model('Products', productSchema);
