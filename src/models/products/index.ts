49e2b5fc5db0c8bfd9c2a51d726052f1e2c4f885
Cesar Ortz <cesarjavierortizmontero@gmail.com>
2021-05-19 23:30:16 -0500

    improving the code structure

--- src/models/products/index.ts
+++ src/models/products/index.ts
@@ -1,6 +1,25 @@
 import { gql } from 'apollo-server-express';
 import { IResolvers } from 'graphql-tools';
 
+interface Product {
+  _id: String;
+  name: String;
+  mark: String;
+  category: String;
+  model: String;
+  price: Number;
+  avalible: Boolean;
+  stock: Number;
+  color: String;
+  shortDescription: String;
+  longDescription: String;
+  weight: Number;
+  warranty: Boolean;
+  timeWarranty: Number;
+  inventory: Boolean;
+  hidden: Boolean;
+}
+
 const typeDefs = gql`
   type Product {
     _id: ID!
@@ -21,59 +40,53 @@ const typeDefs = gql`
     hidden: Boolean
   }
 
+  input ProductInput {
+    name: String
+    mark: String
+    category: String
+    model: String
+    price: Int
+    avalible: Boolean
+    stock: Int
+    color: String
+    shortDescription: String
+    longDescription: String
+    weight: Int
+    warranty: Boolean
+    timeWarranty: Int
+    inventory: Boolean
+    hidden: Boolean
+  }
+
   type Query {
-    AllProducts: [Product]!
+    AllProducts(where: ProductInput): [Product]!
     Product(_id: ID!): Product
     FilteredProducts(filter: String): [Product]
   }
 
   type Mutation {
-    CreateProduct(
-      name: String!
-      mark: String!
-      category: String!
-      model: String
-      price: Int!
-      avalible: Boolean
-      stock: Int!
-      color: String
-      shortDescription: String!
-      longDescription: String
-      weight: Int
-      warranty: Boolean!
-      timeWarranty: Int
-      inventory: Boolean
-      hidden: Boolean
-    ): Product
-
+    CreateProduct(input: ProductInput): Product
     DeleteProduct(_id: ID!): Product
-
-    UpdateProduct(
-      _id: ID!
-      name: String
-      mark: String
-      category: String
-      model: String
-      price: Int
-      avalible: Boolean
-      stock: Int
-      color: String
-      shortDescription: String
-      longDescription: String
-      weight: Int
-      warranty: Boolean
-      timeWarranty: Int
-      inventory: Boolean
-      hidden: Boolean
-    ): Product
   }
 `;
 
 const resolvers: IResolvers = {
   Query: {
-    AllProducts: async (_parent, _args, { Product }) => {
-      const products = await Product.find();
-      return products.map((el: [any]) => {
+    AllProducts: async (_parent, { where }, { Product }) => {
+      let products;
+      if (where) {
+        const { name } = where;
+        if (name) {
+          products = await Product.find({
+            name: { $regex: new RegExp(name), $options: 'i' },
+          });
+          return products.map((el: Product) => {
+            return el;
+          });
+        }
+      }
+      products = await Product.find();
+      return products.map((el: Product) => {
         return el;
       });
     },
@@ -82,31 +95,23 @@ const resolvers: IResolvers = {
       const product = await Product.findById(_id);
       return product;
     },
-    FilteredProducts: async (_parent, { filter }, { Product }) => {
-      const query = JSON.parse(filter);
-      const products = await Product.find(query);
-      return products.map((el: [any]) => {
-        return el;
-      });
-    },
   },
   Mutation: {
     CreateProduct: async (_parent, args, { Product }) => {
-      const product = await new Product(args).save();
+      const { input } = args;
+      const product = await new Product(input).save();
       return product;
     },
-    DeleteProduct: async (_parent, args, { Product }) => {
-      const { _id } = args;
+    DeleteProduct: async (_parent, { _id }, { Product }) => {
       const product = await Product.findById(_id);
       await Product.deleteOne({ _id });
       return product;
     },
     UpdateProduct: async (_parent, args, { Product }) => {
-      const { _id } = args;
+      const { _id, input } = args;
       let product = await Product.findById(_id);
       if (product) {
-        delete args._id;
-        await Product.updateOne({ _id }, args);
+        await Product.updateOne({ _id }, input);
         product = await Product.findById(_id);
         return product;
       }
